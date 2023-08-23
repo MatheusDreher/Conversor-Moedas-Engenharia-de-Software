a8ce57f9fba043ae64fd09491d543660865d0347
MatheusDreher <matheusdreher11@gmail.com>
2023-08-23 01:34:25 -0300

    Adição do euro para real

---
+++ codigo.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+const float taxaDolarEuro = 0.92;
+const float taxaEuroReal = 5.36;
+const float taxaRealDolar = 0.20;
+
+// Função para realizar a conversão
+float converter(float valor, float taxa) {
+    return valor * taxa;
+}
+
+int main() {
+    int escolha;
+    float valor;
+    int continuar = 1; 
+
+    printf("Bem-vindo ao Conversor de Moedas!\n");
+
+    while (continuar) {
+        printf("Selecione a operacao:\n");
+        printf("1. Dolar para Euro\n");
+        printf("2. Euro para Real\n");
+        printf("3. Real para Dolar\n");
+        printf("4. Sair\n");
+        scanf("%d", &escolha);
+
+        switch (escolha) {
+            case 1:
+                printf("Digite o valor em Dolares: ");
+                scanf("%f", &valor);
+                printf("%.2f Dolares equivalem a %.2f Euros.\n", valor, converter(valor, taxaDolarEuro));
+                break;
+          
+            case 2:
+                printf("Digite o valor em Euros: ");
+                scanf("%f", &valor);
+                printf("%.2f Euros equivalem a %.2f Reais.\n", valor, converter(valor, taxaEuroReal));
+                break;
+
+                default:
+                printf("Opcao invalida.\n");
+                break;
+        }
+    }
+
+    return 0;
+}
